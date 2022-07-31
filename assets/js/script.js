6f39cd3c16fc0f21976e45efeed4007c19fdfd7d
Kayla S <kaylaesmith1@gmail.com>
2022-07-31 14:36:14 +0000

    Add first function to submit button and type of math game. Function tested and working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// Wait for DOM to finsih loading before running the game--otherwise maybe it loads before everything is ready //
+// Get button elements and add event listeners to them //
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    //instead of the below, a cleaner version can be used: "let button of buttons"//
+    // for (let i=0; i < buttons.length; i++){}   
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type")==="submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+
+            }
+
+        })
+    }
+    
+
+})
+
 function runGame() {
 
 }
