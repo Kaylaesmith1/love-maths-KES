e7c0e1a2f32e5e1fef79654ba2b09a08ee27b962
Kayla S <kaylaesmith1@gmail.com>
2022-08-01 11:26:47 +0000

    Create addition question, error messages for other operations, console message when error.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                //include the alert right off the bat to show the function is working //
+                // alert(`You clicked ${gameType}`);
+                runGame(gameType);
 
             }
 
         })
     }
+    runGame("addition");
     
 
 })
@@ -25,10 +28,20 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * The main game "loop", called when the script is first loaded and after the user's answer has been processed.  
  */
-function runGame() {
+
+//using docstrings will show the sentence of what your code does if your function is located somewhere else in the code too //
+function runGame(gameType) {
 // These two variables generate random numbers betwen 1 and 25 //
     let num1 = Math.floor(Math.random()*25) + 1;
     let num2 = Math.floor(Math.random()*25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion (num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        // the 'throw' will send this message to the console >> open it and you can see the error //
+        throw `Unknown game type: ${gameType}. Aborting.`;
+    }
 
 }
 
@@ -52,7 +65,13 @@ function checkAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent= "+";
+
+
 
 }
 
